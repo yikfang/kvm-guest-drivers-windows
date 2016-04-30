739776ac5e865b32ebf90116ea079d15892cc17e
Vadim Rozenfeld <vrozenfe@redhat.com>
2016-04-29 20:38:11 -0700

    [balloon] fix compilation problems

--- Balloon/app/buildOne.bat
+++ Balloon/app/buildOne.bat
@@ -1,53 +1,24 @@
 @echo off
-if /i "%1"=="Win7" goto %1_%2
-if /i "%1"=="Win8" goto %1_%2
-if /i "%1"=="Win10" goto %1_%2
-
-if "%DDKVER%"=="" set DDKVER=7600.16385.1
-set BUILDROOT=C:\WINDDK\%DDKVER%
-
-set DDKBUILDENV=
-pushd %BUILDROOT%
-call %BUILDROOT%\bin\setenv.bat %BUILDROOT% %2 fre %1 no_oacr
-popd
-build -cZg
-
-set DDKVER=
-set BUILDROOT=
+if /i "%1"=="WXp" goto %2
+if /i "%1"=="Wnet" goto %2
+if /i "%1"=="Wlh" goto %2
+if /i "%1"=="Win7" goto %2
+if /i "%1"=="Win8" goto %2
+if /i "%1"=="Win10" goto %2
 
 goto :eof
 
-:Win7_x86
+:x86
 if exist "Release\%~2" goto :eof
-call :BuildWin8 "Release|x86" buildfre_x86.log
+call :BuildProject "Release|x86" buildfre_x86.log
 goto :eof
 
-:Win7_x64
+:x64
 if exist "Release\%~2" goto :eof
-call :BuildWin8 "Release|x64" buildfre_amd64.log
+call :BuildProject "Release|x64" buildfre_amd64.log
 goto :eof
 
-:Win8_x86
-if exist "Release\%~2" goto :eof
-call :BuildWin8 "Release|x86" buildfre_x86.log
-goto :eof
-
-:Win8_x64
-if exist "Release\%~2" goto :eof
-call :BuildWin8 "Release|x64" buildfre_amd64.log
-goto :eof
-
-:Win10_x86
-if exist "Release\%~2" goto :eof
-call :BuildWin8 "Release|x86" buildfre_x86.log
-goto :eof
-
-:Win10_x64
-if exist "Release\%~2" goto :eof
-call :BuildWin8 "Release|x64" buildfre_amd64.log
-goto :eof
-
-:BuildWin8
+:BuildProject
 setlocal
 call ..\..\tools\callVisualStudio.bat 14 blnsvr.vcxproj /Rebuild "%~1" /Out %2
 endlocal
