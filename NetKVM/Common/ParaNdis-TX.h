d900100aa6c0607326b4c3e3298b7542a07c654a
Dmitry Fleytman <dmitry@daynix.com>
2016-05-30 11:12:58 +0300

    NetKVM: Drop explicit implementation of default CParaNdisTX::ctor
    Signed-off-by: Dmitry Fleytman <dmitry@daynix.com>

--- NetKVM/Common/ParaNdis-TX.h
+++ NetKVM/Common/ParaNdis-TX.h
@@ -182,7 +182,7 @@ typedef struct _tagSynchronizedContext tSynchronizedContext;
 class CParaNdisTX : public CParaNdisTemplatePath<CTXVirtQueue>, public CNdisAllocatable<CParaNdisTX, 'XTHR'>
 {
 public:
-    CParaNdisTX();
+    CParaNdisTX() = default;
     ~CParaNdisTX();
 
     bool Create(PPARANDIS_ADAPTER Context, UINT DeviceQueueIndex);
